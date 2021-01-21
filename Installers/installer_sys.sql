08678b600bc2b779fc2617dbc45afd6a03037e24
PSFHU <prokszamano@gmail.com>
2021-01-21 21:02:05 +0100

    Organizing installer contents - SQL Edit

--- Installers/installer_sys.sql
+++ Installers/installer_sys.sql
@@ -1,3 +1,12 @@
-PROMPT Creating port_admin...
+PROMPT Creating roles...
+
+@./Roles/Port_Manager.sql
+@./Roles/Ship_Manager.sql
+@./Roles/Site_Manager.sql
+
+PROMPT Creating users...
 
 @./Users/Port_Admin.sql
+@./Users/Port_Manager.sql
+@./Users/Ship_Manager.sql
+@./Users/Site_Manager.sql
