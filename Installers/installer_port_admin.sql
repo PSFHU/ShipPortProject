15870b7dd2033fdfec924cadb1378404bc7d22f0
PSFHU <prokszamano@gmail.com>
2021-01-21 20:30:56 +0100

    Adding roles and users to the installer - SQL Edit

--- Installers/installer_port_admin.sql
+++ Installers/installer_port_admin.sql
@@ -1,3 +1,15 @@
+PROMPT Creating roles...
+
+@./Roles/Port_Manager.sql
+@./Roles/Ship_Manager.sql
+@./Roles/Site_Manager.sql
+
+PROMPT Creating users...
+
+@./Users/Port_Manager.sql
+@./Users/Ship_Manager.sql
+@./Users/Site_Manager.sql
+
 PROMPT Creating tables...
 
 @./Tables/Container_Type.sql
