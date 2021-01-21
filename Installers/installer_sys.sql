831c9ccebf08f013ac1211858f3b2fda2a179291
PSFHU <prokszamano@gmail.com>
2021-01-21 21:15:26 +0100

    Installer extra step added - SQL Edit

--- Installers/installer_sys.sql
+++ Installers/installer_sys.sql
@@ -1,12 +1,3 @@
-PROMPT Creating roles...
-
-@./Roles/Port_Manager.sql
-@./Roles/Ship_Manager.sql
-@./Roles/Site_Manager.sql
-
-PROMPT Creating users...
+PROMPT Creating port admin...
 
 @./Users/Port_Admin.sql
-@./Users/Port_Manager.sql
-@./Users/Ship_Manager.sql
-@./Users/Site_Manager.sql
---
+++ Installers/installer_sys_finish.sql
@@ -0,0 +1,11 @@
+PROMPT Creating roles...
+
+@./Roles/Port_Manager.sql
+@./Roles/Ship_Manager.sql
+@./Roles/Site_Manager.sql
+
+PROMPT Creating users...
+
+@./Users/Port_Manager.sql
+@./Users/Ship_Manager.sql
+@./Users/Site_Manager.sql
